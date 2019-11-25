265604459da397002355df2074c72ee4f1ea4fae
mahao2 <2727485259@qq.com>
2019-11-25 20:57:18 +0800

    更改

--- web-jsp/src/main/java/cn/mh/sb/webjsp/WebJspApplication.java
+++ web-jsp/src/main/java/cn/mh/sb/webjsp/WebJspApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebJspApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(WebJspApplication.class, args);
+    public static void main(String[] arg) {
+        SpringApplication.run(WebJspApplication.class, arg);
     }
 
 }
