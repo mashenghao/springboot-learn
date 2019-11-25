deae8265e33b4300f2860bb6efa757424b54d89d
mahao2 <2727485259@qq.com>
2019-11-25 20:54:23 +0800

    springboot使用外置的servlet容器，以及如何启动springboot的原理

---
+++ web-jsp/src/main/java/cn/mh/sb/webjsp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package cn.mh.sb.webjsp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebJspApplication.class);
+    }
+
+}
---
+++ web-jsp/src/test/java/cn/mh/sb/webjsp/WebJspApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.mh.sb.webjsp;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebJspApplicationTests {
+
+
+    void contextLoads() {
+    }
+
+}
